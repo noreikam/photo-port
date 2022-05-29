783e6110730d73f3bb4eb25765a7f8285d379e8b
noreikam <noreikam1@gmail.com>
2022-05-28 22:01:49 -0400

    built nav and about components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { capitalizeFirstLetter } from "../../utils/helpers";
+const categories = [
+  {
+    name: "commercial",
+    description: "Photos of grocery stores, food trucks, and other commercial projects"
+  },
+  {
+    name: "portraits",
+    description: "Portraits of people in my life"
+  },
+  {
+    name: "food",
+    description: "Delicious delicacies"
+  },
+  {
+    name: "landscape", 
+    description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+  }
+];
+
+const categorySelected = name => {
+  console.log(`${name} clicked`);
+}
+
+const handleClick = () => {
+  console.log("click handled")
+}
+
+const Nav = () => {
+  return(
+    <header data-testid="header" className="flex-row px-1">
+       <h2>
+        <a href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about" onClick={() => handleClick()}>
+              About Me
+            </a>
+          </li>
+          <li className="mx-2">
+            <span onClick={() => handleClick()}>
+              Contact
+            </span>
+          </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name} >
+              <span onClick={() => categorySelected(category.name)} >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav;
