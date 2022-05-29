7c15fb5b3ae20526aaf78f82aa97640a74df2be6
noreikam <noreikam1@gmail.com>
2022-05-28 21:03:56 -0400

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// THIS FUNCTION DOES NOT WORK
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 ReactDOM.render(
   <React.StrictMode>
 
