02658dcddfb48126c82d7d51146aeb9a44bdd147
noreikam <noreikam1@gmail.com>
2022-05-29 12:01:31 -0400

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import About from './components/About';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
+import About from './components/About';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description: "Photos of grocery stores, food trucks, and other commercial projects"
+    },
+    {
+      name: "portraits",
+      description: "Portraits of people in my life"
+    },
+    {
+      name: "food",
+      description: "Delicious delicacies"
+    },
+    {
+      name: "landscape", 
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature"
+    }
+  ]);
+
+  const[currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav />
-      <main>        
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <Gallery />
         <About />
       </main>
     </div>
